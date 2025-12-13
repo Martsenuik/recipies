93fdde15ee2a2e1f50e4e50c71d03f55decac782
Martseniuk Alina <martseniuk.av@gmail.com>
2025-12-13 13:48:43 +0200

    add

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 
 
-const BASE_PATH = '/recipies/'; 
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
 
 export default defineConfig({
   plugins: [react()],
-  base: BASE_PATH, 
+  
+
+  base: '/recipes/', 
 });
